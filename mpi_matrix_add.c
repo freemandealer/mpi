c877b600ad6228f073a97c2754b1c15c61cf90c4
freeman <freemandealer@live.com>
2014-04-10 16:04:44 +0800

    update mpi_matrix_add.c

--- mpi_matrix_add.c
+++ mpi_matrix_add.c
@@ -4,14 +4,42 @@
 #include "stat.h"
 #include "mpi.h"
 
-int main()
+int main(int argc, char *argv[])
 {
 	int ret;
-	matrix_t* matrixp;
+	int myid;
+	int size;
+	char buf[2048];
+	MPI_Status status;
+	matrix_t* m_matrip1, m_matrip2;
+	int m_row, m_col;  // tow matrixes share the same size
+	int i,j;
+
+	/* Create matrixes */
+	printf("How many rows:\n"); scanf("%d", &m_row);
+	printf("How many columns:\n"); scanf("%d", &m_col);
+	matrip1 = init_matrix(m_row, m_col);
+	matrip2 = init_matrix(m_row, m_col);
+	printf("Enter the matrix1:\n");
+	for (i=0; i<m_row; i++) {
+		for (j=0; j<m_col; j++)
+		scanf("%d", &matrip1[i][j]);
+	}
+	printf("Enter the matrix1:\n");
+	for (i=0; i<m_row; i++) {
+		for (j=0; j<m_col; j++)
+		scanf("%d", &matrip1[i][j]);
+	}
 	
-	matrixp = init_matrix(3, 4);
 	set_matrix_pos_value(matrixp, 2, 1, 3);
-	printf("%lf\n", get_matrix_pos_value_ret(matrixp, 2, 1));
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+	/* Divide matrix */
+	
+	MPI_Finalize();
 	return 0;
 }
 
